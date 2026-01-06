41ad6cc14e8256d22a89cc36ecb9445143c7dd61
euwpc <europeanwindstormpredictioncen@gmail.com>
2026-01-06 22:15:37 +0200

    Update update_map.py

--- update_map.py
+++ update_map.py
@@ -126,7 +126,7 @@ for view_key, view_conf in views.items():
 
     for var_key, conf in variables.items():
         
-      data = get_analysis(conf['var'])
+    data = get_analysis(conf['var'])
 
 
         # Min/max only in Baltic region
